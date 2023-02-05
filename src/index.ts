88a6212f3b0376cfe046cea17042205423ae8ce1
Alexander <alexanderkratexor@gmail.com>
2023-02-05 17:36:43 +0200

    feat: update index.html, index.ts, style.scss

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
+import './normalize.css'
 import './style.scss';
-
-console.log('Hello world');
+const main = document.querySelector('.main__container');
+const addGame = document.querySelector('.main__button');
+addGame?.addEventListener('click', ()=>{
+  console.log(main)
+let Data:{gameName:string;description:string} = {
+  gameName: 'gameName',
+  description: 'someText'
+}
+  const html = `
+  <div class="game-container__image"></div>
+  <div class="game-container__block">
+    <div class="game-container__title">${Data['gameName']}</div>
+    <div class="game-container__description">${Data['description']}</div>
+ `;
+  const root = document.createElement('div');
+  root.classList.add('game-container')
+  root.innerHTML = html;
+  main?.appendChild(root);
+})
