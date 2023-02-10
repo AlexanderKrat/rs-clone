2e033f8a113b7d4ae4cc03c67f1551dc00b723fc
Alex Hramovich <xtr1m@mail.ru>
2023-02-10 12:36:38 +0300

    feat: add time and clicks

--- src/assets/components/sticks/paper.ts
+++ src/assets/components/sticks/paper.ts
@@ -13,11 +13,23 @@ const verbs = [
 
 export class Sticks {
     verbs: string[][] | undefined;
-    constructor(obj?: string[][]) {
+    name: string | undefined;
+    title: string | undefined;
+    constructor(obj?: string[][], name?: string, title?: string) {
         if (obj) {
             this.verbs = obj;
         } else {
             this.verbs = verbs;
+        }
+        if (name) {
+            this.name = name;
+        } else {
+            this.name = 'Stickers';
+        }
+        if (title) {
+            this.title = title;
+        } else {
+            this.title = 'Compare irregular verbs';
         }
     }
     create() {
@@ -50,30 +62,40 @@ export class Sticks {
         const numberOfSticks = this.verbs?.length;
         let winCond = 0;
         let clicked: HTMLElement[] = [];
+        let countClicks = 1;
         sticksGame.addEventListener('click', (e) => {
             const block = e.target as HTMLElement;
             if (block.className === 'sticksText' && block.style.opacity === '0') {
+              this.startTime(countClicks)
+              countClicks++
+              block.style.opacity = '1'
+              clicked.push(block)
                 if (clicked.length > 1) {
                     const finded = verbs.find(element => clicked[0].innerHTML === element[0] || clicked[0].innerHTML === element[1])
                     if (finded![1] === clicked[1].innerHTML && finded![0] === clicked[0].innerHTML || finded![0] === clicked[1].innerHTML && finded![1] === clicked[0].innerHTML) {
+                        clicked.forEach(el => {
+                            el.classList.add('win-anim')
+                            setTimeout(() => {
+                              el.classList.remove('win-anim')
+                            }, 1500);
+                        })
                         clicked = []
                         winCond++;
-                        console.log(winCond, numberOfSticks)
                         if (winCond === numberOfSticks) {
                             alert('win!')
                         }
                     } else {
                         clicked.forEach(el => {
-                            el.style.opacity = '0'
+                            el.classList.add('stickers-anim')
+                            setTimeout(() => {
+                              el.style.opacity = '0'
+                              el.classList.remove('stickers-anim')
+                            }, 1500);
                         })
                         clicked = []
                     }
                 }
-                block.style.opacity = '1'
-                clicked.push(block)
             }
-            
-            
         })
     }
     randomizer() {
@@ -85,10 +107,46 @@ export class Sticks {
         const shuffledArray = arraOfSticks.sort((a, b) => 0.5 - Math.random());
         const sticksGame = document.querySelector('.sticks-place') as HTMLElement;
         sticksGame.innerHTML = '';
+        const statsSticks = document.createElement('div')
+        const startedTime = document.createElement('div')
+        const countClicks = document.createElement('div')
+        statsSticks.classList.add('stats-sticks')
+        sticksGame.appendChild(statsSticks)
+        statsSticks.appendChild(startedTime)
+        statsSticks.appendChild(countClicks)
+        startedTime.innerHTML = `Time: ${0}`
+        countClicks.innerHTML = `Clicks: ${0}`
+        countClicks.classList.add('clicked-sticks')
+        startedTime.classList.add('time-sticks')
+        const divForSticks = document.createElement('div')
+        sticksGame.appendChild(divForSticks)
+        divForSticks.classList.add('sticks-place')
         shuffledArray.forEach((ele) => {
-            sticksGame.appendChild(ele)
+          divForSticks.appendChild(ele)
         })
     }
+    startTime(clicks: number) {
+      let timeCount = 0;
+      let minutes = 0;
+      if (clicks === 1) {
+        setInterval(() => {
+          timeCount++
+          if (timeCount < 60) {
+            document.querySelector('.time-sticks')!.innerHTML = `Time: ${timeCount}`
+          } else {
+            minutes = Math.floor(timeCount / 60)
+            let seconds = timeCount % 60;
+            if (seconds < 10) {
+              document.querySelector('.time-sticks')!.innerHTML = `Time: ${minutes}:0${seconds}`
+            } else {
+              document.querySelector('.time-sticks')!.innerHTML = `Time: ${minutes}:${seconds}`
+
+            }
+          }
+          
+        }, 1000);
+      }
+      document.querySelector('.clicked-sticks')!.innerHTML = `Clicks: ${clicks}`
     
-    
+    }
 }
