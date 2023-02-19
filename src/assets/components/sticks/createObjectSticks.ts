2450cb0a34fc0822a76a2f5f4df3940e0f0183e1
Alex Hramovich <xtr1m@mail.ru>
2023-02-19 10:40:29 +0300

    fix: eslint

--- src/assets/components/sticks/createObjectSticks.ts
+++ src/assets/components/sticks/createObjectSticks.ts
@@ -1,17 +1,17 @@
-import createSticksFromForm from './createSticksFromForm'
+import createSticksFromForm from './createSticksFromForm';
 
 function createSticks() {
-    const inputs = document.querySelectorAll('.input-in-form')
-        let readValues = 0;
-        inputs.forEach(el => {
-            const check = el as HTMLInputElement;
-            if (check.value) {
-                readValues++
-            }
-        })
-        if (readValues === inputs.length) {
-            createSticksFromForm()
-        }
+  const inputs = document.querySelectorAll('.input-in-form');
+  let readValues = 0;
+  inputs.forEach((el) => {
+    const check = el as HTMLInputElement;
+    if (check.value) {
+      readValues++;
+    }
+  });
+  if (readValues === inputs.length) {
+    createSticksFromForm();
+  }
 }
 
-export default createSticks
+export default createSticks;
--- src/assets/components/sticks/createSticksFromForm.ts
+++ src/assets/components/sticks/createSticksFromForm.ts
@@ -1,33 +1,33 @@
-import { Sticks } from './paper'
-import openSection from '../openSection'
-import createUrlSticks from './addUrlSticks'
-import createCell from '../createCell'
+import { Sticks } from './paper';
+import openSection from '../openSection';
+import createUrlSticks from './addUrlSticks';
+import createCell from '../createCell';
 
 function createSticksFromForm() {
-    const arr: string[][] = []
-    const lines = document.querySelectorAll('.sticks-form-line')
-    lines.forEach(el => {
-        const insideArr: string[] = []
-        const first = el.firstElementChild as HTMLInputElement;
-        const fir = first.value
-        const second = el.firstElementChild?.nextElementSibling as HTMLInputElement;
-        const sec = second.value
-        insideArr.push(fir)
-        insideArr.push(sec)
-        arr.push(insideArr)
-    })
-    
-    const title = document.querySelector('.input-name-sticks') as HTMLInputElement
+  const arr: string[][] = [];
+  const lines = document.querySelectorAll('.sticks-form-line');
+  lines.forEach((el) => {
+    const insideArr: string[] = [];
+    const first = el.firstElementChild as HTMLInputElement;
+    const fir = first.value;
+    const second = el.firstElementChild?.nextElementSibling as HTMLInputElement;
+    const sec = second.value;
+    insideArr.push(fir);
+    insideArr.push(sec);
+    arr.push(insideArr);
+  });
 
-    const newSticks = new Sticks(arr, title.value)
-    newSticks.create();
-    newSticks.check();
-    newSticks.randomizer();
+  const title = document.querySelector('.input-name-sticks') as HTMLInputElement;
 
-    createUrlSticks(arr, title.value)
-    openSection('sticks-place')
-    // console.log(window.location.hash.split('/')[1])
-    createCell(window.location.hash.split('/')[1])
+  const newSticks = new Sticks(arr, title.value);
+  newSticks.create();
+  newSticks.check();
+  newSticks.randomizer();
+
+  createUrlSticks(arr, title.value);
+  openSection('sticks-place');
+  // console.log(window.location.hash.split('/')[1])
+  createCell(window.location.hash.split('/')[1]);
 }
 
-export default createSticksFromForm
+export default createSticksFromForm;
